5f8942f07c94d6701d7196ee5f2491efa241f7ff
Asterius8 <oscar.vg@tecjerez.edu.mx>
2026-03-17 11:36:00 -0600

    Ajustes para conexion con clever cloud

--- config/database.js
+++ config/database.js
@@ -2,14 +2,18 @@
 const mysql = require('mysql2');
 
 const conexion = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'oscar08',
-    database: 'db_Express_2026',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-conexion.connect(function(err){
-    if(err)
+conexion.connect(function (err) {
+    if (err)
         throw err;
     console.log('Conexion a la base de datos exitosa :D');
 });
